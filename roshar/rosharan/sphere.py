fd80f3f6d29915215250864848a5eca96cc9bd56
Adrian Miguel <adriantmiguel@gmail.com>
2024-02-07 20:28:28 -0800

    made spheres

--- roshar/rosharan/sphere.py
+++ roshar/rosharan/sphere.py
@@ -1,12 +1,4 @@
 from enum import Enum
-from typing import Mapping
-
-# Each sphere has a:
-# - gem size: chip, mark, broam
-# - gem type: diamond, garnet, etc.
-
-# Each gem type is associated with a different tier:
-# - cheapest, less weight, etc
 
 
 # From https://coppermind.net/wiki/Spheres#Value
@@ -45,18 +37,19 @@ class RosharanGemType(Enum):
     def __init__(self, tier: RosharanGemTier) -> None:
         self.tier = tier
 
-    Amethyst = RosharanGemTier.PRIME_PAIR
-    Diamond = RosharanGemTier.CHEAPEST
-    Emerald = RosharanGemTier.HIGHEST
-    Garnet = RosharanGemTier.LESS_WEIGHT
-    Heliodor = RosharanGemTier.LESS_WEIGHT
-    Ruby = RosharanGemTier.MIDDLE_WEIGHT
-    Sapphire = RosharanGemTier.PRIME_PAIR
-    Smokestone = RosharanGemTier.MIDDLE_WEIGHT
-    Topaz = RosharanGemTier.LESS_WEIGHT
-    Zircon = RosharanGemTier.MIDDLE_WEIGHT
+    AMETHYST = RosharanGemTier.PRIME_PAIR
+    DIAMOND = RosharanGemTier.CHEAPEST
+    EMERALD = RosharanGemTier.HIGHEST
+    GARNET = RosharanGemTier.LESS_WEIGHT
+    HELIODOR = RosharanGemTier.LESS_WEIGHT
+    RUBY = RosharanGemTier.MIDDLE_WEIGHT
+    SAPPHIRE = RosharanGemTier.PRIME_PAIR
+    SMOKESTONE = RosharanGemTier.MIDDLE_WEIGHT
+    TOPAZ = RosharanGemTier.LESS_WEIGHT
+    ZIRCON = RosharanGemTier.MIDDLE_WEIGHT
 
 
+# From https://coppermind.net/wiki/Spheres#Value
 class RosharanSphere(Enum):
     def __new__(cls, *args):
         value = len(cls.__members__) + 1
@@ -64,28 +57,50 @@ class RosharanSphere(Enum):
         obj._value_ = value
         return obj
 
-    def __init__(self, suffix):
-        self.suffix = suffix
+    def __init__(
+        self,
+        gem_type: RosharanGemType,
+        gem_size: RosharanGemSize,
+    ) -> None:
+        self.gem_type = gem_type
+        self.gem_size = gem_size
 
-    # From https://coppermind.net/wiki/Spheres#Value
-    Jes = "es"
-    Nan = "an"
-    Chach = "ach"
-    Vev = "ev"
-    Palah = "ah"
-    Shash = "ash"
-    Betab = "ab"
-    Kak = "ak"
-    Tanat = "at"
-    Ishi = "ish"
+    AMETHYST_CHIP = RosharanGemType.AMETHYST, RosharanGemSize.CHIP
+    AMETHYST_MARK = RosharanGemType.AMETHYST, RosharanGemSize.MARK
+    AMETHYST_BROAM = RosharanGemType.AMETHYST, RosharanGemSize.BROAM
 
+    CLEARCHIP = RosharanGemType.DIAMOND, RosharanGemSize.CHIP
+    CLEARMARK = RosharanGemType.DIAMOND, RosharanGemSize.MARK
+    DIAMOND_BROAM = RosharanGemType.DIAMOND, RosharanGemSize.BROAM
 
-ROSHARAN_NUMBERS_BY_VALUE: Mapping[int, RosharanNumber] = {
-    number.value: number
-    for number in RosharanNumber
-}
+    EMERALD_CHIP = RosharanGemType.EMERALD, RosharanGemSize.CHIP
+    EMERALD_MARK = RosharanGemType.EMERALD, RosharanGemSize.MARK
+    EMERALD_BROAM = RosharanGemType.EMERALD, RosharanGemSize.BROAM
 
-ROSHARAN_NUMBERS_BY_NAME: Mapping[str, RosharanNumber] = {
-    number.name: number
-    for number in RosharanNumber
-}
+    GARNET_CHIP = RosharanGemType.GARNET, RosharanGemSize.CHIP
+    GARNET_MARK = RosharanGemType.GARNET, RosharanGemSize.MARK
+    GARNET_BROAM = RosharanGemType.GARNET, RosharanGemSize.BROAM
+
+    HELIODOR_CHIP = RosharanGemType.HELIODOR, RosharanGemSize.CHIP
+    HELIODOR_MARK = RosharanGemType.HELIODOR, RosharanGemSize.MARK
+    HELIODOR_BROAM = RosharanGemType.HELIODOR, RosharanGemSize.BROAM
+
+    FIRECHIP = RosharanGemType.RUBY, RosharanGemSize.CHIP
+    FIREMARK = RosharanGemType.RUBY, RosharanGemSize.MARK
+    RUBY_BROAM = RosharanGemType.RUBY, RosharanGemSize.BROAM
+
+    SAPPHIRE_CHIP = RosharanGemType.SAPPHIRE, RosharanGemSize.CHIP
+    SAPPHIRE_MARK = RosharanGemType.SAPPHIRE, RosharanGemSize.MARK
+    SAPPHIRE_BROAM = RosharanGemType.SAPPHIRE, RosharanGemSize.BROAM
+
+    SMOKESTONE_CHIP = RosharanGemType.SMOKESTONE, RosharanGemSize.CHIP
+    SMOKESTONE_MARK = RosharanGemType.SMOKESTONE, RosharanGemSize.MARK
+    SMOKESTONE_BROAM = RosharanGemType.SMOKESTONE, RosharanGemSize.BROAM
+
+    TOPAZ_CHIP = RosharanGemType.TOPAZ, RosharanGemSize.CHIP
+    TOPAZ_MARK = RosharanGemType.TOPAZ, RosharanGemSize.MARK
+    TOPAZ_BROAM = RosharanGemType.TOPAZ, RosharanGemSize.BROAM
+
+    ZIRCON_CHIP = RosharanGemType.ZIRCON, RosharanGemSize.CHIP
+    ZIRCON_MARK = RosharanGemType.ZIRCON, RosharanGemSize.MARK
+    ZIRCON_BROAM = RosharanGemType.ZIRCON, RosharanGemSize.BROAM
